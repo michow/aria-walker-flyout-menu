9ecad370b8046c1af5152f4be8e91ba5a184e4d6
michow <michow@smalltalk.agency>
2020-07-15 14:13:18 -0700

    initial

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# aria-walker-flyout-menu
+# WAI-ARIA WordPress Walker
+
+Custom nav menu walker that outputs ARIA attributes for a navigation with flyout submenus. Code builds upon the [WAI-ARIA WordPress Walker](https://github.com/proteusthemes/WAI-ARIA-Walker_Nav_Menu) done by [primozcigler](https://github.com/primozcigler).
+
+The walker outputs the recommended accessibility design pattern for [flyout menus](https://www.w3.org/WAI/tutorials/menus/flyout/) and [navigation menu buttons](https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html) from [w3.org](https://www.w3.org/). 
+
+See [my demo here](https://codepen.io/michow/pen/Koojpp).
+
+## Usage
+
+1. Copy the code from `aria-walker-flyout-menu.php` into `/<path-to-wp>/wp-content/themes/<theme-name>/functions.php`
+2. Instantiate `Aria_Walker_Flyout_Menu` and pass it as an argument to `wp_nav_menu()` through the `wp_nav_menu` array
+
+		<nav class="your-class" aria-label="Main Menu">
+			<?php
+				wp_nav_menu( array(
+					'theme_location' => 'your_menu_name_here',
+					'depth'          => 2,
+					'container'      => false,
+					'menu_class'     => 'menu-top-nav',
+					'walker'         => new Aria_Walker_Flyout_Menu(),
+					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+				) );
+			?>
+		</nav>
+	  
+> **Note:** You will need to include the appropriate js and css to toggle ARIA attributes and control the button. View my demo for example code.
+
+## Sample Output
+ 
+For any parent link that has a submenu, it will render a `button` with its `aria-controls` value matching the `id` of the submenu it triggers. It will also output the necessary `aria-haspopup` and `aria-expanded` attributes.
+ 
+		<nav class="your-class" aria-label="Main Menu">
+			<ul id="menu-top-nav" class="menu-top-nav" role="menubar">
+				<li id="menu-item-111" class="menu-item menu-item-has-children...">
+					<a href="#" role="menuitem" aria-haspopup="true" aria-expanded="false">List 1</a>
+					<button aria-expanded="false" aria-controls="submenu-111">
+						<span class="sr-only">Toggle submenu</span>
+					</button>
+					<ul role="menu" class="sub-menu" id="submenu-111">
+						<li id="menu-item-112" class="menu-item...">
+							<a href="#" role="menuitem">List 1 Item 1</a>
+						</li>
+						<li id="menu-item-113" class="menu-item...">
+							<a href="#" role="menuitem">List 1 Item 2</a>
+						</li>
+					</ul>
+				</li>
+				<li id="menu-item-679" class="menu-item menu-item-211">
+					<a href="#" role="menuitem">List 2</a>
+				</li>
+			</ul>
+		</nav>
---
+++ aria-walker-flyout-menu.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * WAI-ARIA Navigation Menu template functions
+ * https://github.com/michow/aria-walker-flyout-menu/blob/master/aria-walker-flyout-menu.php
+ */
+
+/**
+ * Create HTML list of nav menu items.
+ *
+ * @since 1.0.0
+ * @uses Walker
+ * @uses Walker_Nav_Menu
+ */
+class Aria_Walker_Flyout_Menu extends Walker_Nav_Menu {
+  /**
+   * Start the element output.
+   *
+   * @see Walker_Nav_Menu::start_el() for parameters and longer explanation
+   */
+
+  /*
+    Store current menu item ID to use for child submenu html id
+  */
+  private $curItem;
+
+  public function start_lvl(&$output, $depth = 0, $args = []) {
+    $indent = str_repeat("\t", $depth);
+    
+    if ($depth === 0) :
+        $output .= "\n$indent" . '<ul role="menu" class="sub-menu" id="submenu-' . $this->curItem->ID . '">' . "\n";
+    endif;
+  }
+
+  public function end_lvl(&$output, $depth = 0, $args = []) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "$indent</ul>\n";
+  }
+
+  public function start_el(&$output, $item, $depth = 0, $args = [], $id = 0) {
+    $this->curItem = $item;
+    $indent = ($depth) ? str_repeat("\t", $depth) : '';
+
+    $classes = empty($item->classes) ? [] : (array) $item->classes;
+    $classes[] = 'menu-item-' . $item->ID;
+
+    /**
+     * Filter the arguments for a single nav menu item.
+     *
+     * @since 4.4.0
+     *
+     * @param array  $args  An array of arguments.
+     * @param object $item  Menu item data object.
+     * @param int    $depth Depth of menu item. Used for padding.
+     */
+    $args = apply_filters('nav_menu_item_args', $args, $item, $depth);
+
+    /**
+     * Filter the CSS class(es) applied to a menu item's list item element.
+     *
+     * @since 3.0.0
+     * @since 4.1.0 The `$depth` parameter was added.
+     *
+     * @param array  $classes The CSS classes that are applied to the menu item's `<li>` element.
+     * @param object $item    The current menu item.
+     * @param array  $args    An array of {@see wp_nav_menu()} arguments.
+     * @param int    $depth   Depth of menu item. Used for padding.
+     */
+    $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args, $depth));
+    $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+
+    /**
+     * Filter the ID applied to a menu item's list item element.
+     *
+     * @since 3.0.1
+     * @since 4.1.0 The `$depth` parameter was added.
+     *
+     * @param string $menu_id The ID that is applied to the menu item's `<li>` element.
+     * @param object $item    The current menu item.
+     * @param array  $args    An array of {@see wp_nav_menu()} arguments.
+     * @param int    $depth   Depth of menu item. Used for padding.
+     */
+    $id = apply_filters('nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth);
+    $id = $id ? ' id="' . esc_attr($id) . '"' : '';
+
+    $output .= sprintf('%s<li%s%s>', $indent, $id, $class_names);
+
+    /**
+     * Corrected <a> tag aria attributes from original file
+     * Removed tab index values and role
+    */
+
+    $atts = [];
+    $atts['title']  = !empty($item->attr_title) ? $item->attr_title : '';
+    $atts['target'] = !empty($item->target) ? $item->target : '';
+    $atts['rel']    = !empty($item->xfn) ? $item->xfn : '';
+    $atts['href']   = !empty($item->url) ? $item->url : '';
+    $atts['role']   = 'menuitem';
+
+    if ($args->walker->has_children) :
+      $atts['aria-haspopup'] = "true";
+      $atts['aria-expanded'] = "false";
+    endif;
+
+    /**
+     * Filter the HTML attributes applied to a menu item's anchor element.
+     *
+     * @since 3.6.0
+     * @since 4.1.0 The `$depth` parameter was added.
+     *
+     * @param array $atts {
+     *     The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+     *
+     *     @type string $title  Title attribute.
+     *     @type string $target Target attribute.
+     *     @type string $rel    The rel attribute.
+     *     @type string $href   The href attribute.
+     * }
+     * @param object $item  The current menu item.
+     * @param array  $args  An array of {@see wp_nav_menu()} arguments.
+     * @param int    $depth Depth of menu item. Used for padding.
+     */
+    $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args, $depth);
+
+    $attributes = '';
+    foreach ($atts as $attr => $value) {
+      if (!empty($value)) {
+        $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
+        $attributes .= ' ' . $attr . '="' . $value . '"';
+      }
+    }
+
+    /** This filter is documented in wp-includes/post-template.php */
+    $title = apply_filters('the_title', $item->title, $item->ID);
+
+    /**
+     * Filter a menu item's title.
+     *
+     * @since 4.4.0
+     *
+     * @param string $title The menu item's title.
+     * @param object $item  The current menu item.
+     * @param array  $args  An array of {@see wp_nav_menu()} arguments.
+     * @param int    $depth Depth of menu item. Used for padding.
+     */
+    $title = apply_filters('nav_menu_item_title', $title, $item, $args, $depth);
+
+    $item_output = $args->before;
+    $item_output .= '<a'. $attributes .'>';
+    $item_output .= $args->link_before . $title . $args->link_after;
+    $item_output .= '</a>';
+
+    if ($args->walker->has_children) :
+      $item_output .= '<button aria-expanded="false" aria-controls="submenu-' . $item->ID . '" ><span class="sr-only">Toggle submenu</span></button>';
+    endif;
+
+    $item_output .= $args->after;
+
+    /**
+     * Filter a menu item's starting output.
+     *
+     * The menu item's starting output only includes `$args->before`, the opening `<a>`,
+     * the menu item's title, the closing `</a>`, and `$args->after`. Currently, there is
+     * no filter for modifying the opening and closing `<li>` for a menu item.
+     *
+     * @since 3.0.0
+     *
+     * @param string $item_output The menu item's starting HTML output.
+     * @param object $item        Menu item data object.
+     * @param int    $depth       Depth of menu item. Used for padding.
+     * @param array  $args        An array of {@see wp_nav_menu()} arguments.
+     */
+    $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+  }
+}
